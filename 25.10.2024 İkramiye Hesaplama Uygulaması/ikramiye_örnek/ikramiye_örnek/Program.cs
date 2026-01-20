6b97667c74600c40a7eed0e7420e04774a85eef5
Dewstok <procodderx@gmail.com>
2026-01-20 19:36:27 +0300

    Add files via upload

---
+++ 25.10.2024 İkramiye Hesaplama Uygulaması/ikramiye_örnek/ikramiye_örnek/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ikramiye_örnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogruIsim = "Şebnem";
+            string dogruSifre = "1234";
+
+            Console.Write("Kullanıcı Adı: ");
+            string isim = Console.ReadLine();
+
+            Console.Write("Şifre: ");
+            string sifre = Console.ReadLine();
+
+           
+            if (isim == dogruIsim && sifre == dogruSifre)
+            {
+                
+                Console.Write("Yaş: ");
+                int yas = int.Parse(Console.ReadLine());
+
+                Console.Write("Prim günü girin: ");
+                int primGunu = int.Parse(Console.ReadLine());
+
+                
+                if (yas > 40 && primGunu > 5000)
+                {
+                    Console.WriteLine("Emekliliğe hak kazandınız!");
+
+                    
+                    Console.Write("Maaş girin: ");
+                    double maas = double.Parse(Console.ReadLine());
+
+                    Console.Write("Toplam çalışma yılı girin: ");
+                    int calismaYili = int.Parse(Console.ReadLine());
+
+                    
+                    double ikramiye;
+
+                    if (calismaYili > 20)
+                    {
+                        ikramiye = maas * calismaYili * 0.775; // %77,5 ikramiye
+                    }
+                    else
+                    {
+                        ikramiye = maas * calismaYili * 0.505; // %50,5 ikramiye
+                    }
+
+                    Console.WriteLine("Toplam İkramiye: " + ikramiye);
+                }
+                else
+                {
+                    
+                    int eksikGun = 5000 - primGunu;
+
+                    Console.WriteLine("Emeklilik için " + eksikGun + " gün gerekli!");
+
+                    
+
+                    Console.Write("Prim borcu ödemek istiyor musunuz? (Evet/Hayır): ");
+                    string odeme = Console.ReadLine();
+
+                    bool borcOdendi = false;
+
+                    if (odeme.ToLower() == "evet")
+                    {
+                        
+                        double toplamOdeme = 50000;
+                        double taksitTutari = toplamOdeme / 5;
+
+                        Console.WriteLine("Toplam borç: " + toplamOdeme + " TL");
+                        Console.WriteLine("Aylık taksit: " + taksitTutari + " TL (5 taksit)");
+
+                        borcOdendi = true; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("Prim borcu ödenmedi!");
+                    }
+
+                    
+                    if (borcOdendi)
+                    {
+                        Console.WriteLine("Prim borcu ödendi, emekliliğe hak kazandınız!");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hatalı kullanıcı adı veya şifre!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 25.10.2024 İkramiye Hesaplama Uygulaması/ikramiye_örnek/ikramiye_örnek/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ikramiye_örnek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ikramiye_örnek")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c2a26450-e590-46b3-99c7-9ab7f185c8a6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
