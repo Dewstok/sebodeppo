46d1ed0a2d4382b16d100c4e354319c322057a43
Dewstok <procodderx@gmail.com>
2026-01-20 19:49:43 +0300

    Add files via upload

---
+++ 11.10.2024 Sayı Tahmin Oyunu/k1/k1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rastgele sayı 
+            Random rastgele = new Random();
+            int rastgeleSayi = rastgele.Next(1, 100); // 1-20 arasında rastgele bir sayı üretiyoruz.
+
+            // Değişkenler
+            int tahmin = 0;
+            int denemeSayisi = 0;
+
+            Console.WriteLine("1-100 arasında tuttuğum sayıyı bil bakalım!");
+
+            //doğru tahmin yapana kadar döngü devam...
+            while (tahmin != rastgeleSayi)
+                
+                {
+                Console.Write("Tahmin gir: ");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+                denemeSayisi++;
+
+                if (tahmin < rastgeleSayi)
+                {
+                    Console.WriteLine("büyük bir sayı dene!");
+                }
+
+                else if (tahmin > rastgeleSayi)
+                {
+                    Console.WriteLine("küçük bir sayı dene!");
+                }
+            }
+
+            // Doğru tahmin
+            Console.WriteLine("..................................");
+            Console.WriteLine("bildin,tebrikler! " + denemeSayisi + " . sayıda doğru tahmin ettin!");
+            Console.ReadLine();
+        }
+    }
+}
